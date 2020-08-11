389f15f8fd662813d904dbde84877fa497ffc6d5
Fernando Flores <nbafernandoflores8@gmail.com>
2020-08-11 12:13:35 -0600

    onclick events

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 const Square = ({value}: { value: number }) => (
-    <button className="square">
+    <button className="square" onClick={() => alert('click')}>
         {value}
     </button>
 );
